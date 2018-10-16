45b449dd12e81f1a9db68d4b750681b40c283c9e
myoui <phoenixxturtle@gmail.com>
2018-10-15 21:00:33 -0700

    Updates

--- precache-manifest.ddfc32f94f546fbfcd4ec816ef4cec94.js
+++ precache-manifest.ddfc32f94f546fbfcd4ec816ef4cec94.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fetchnews/static/js/runtime~main.9ba95e36.js"
   },
   {
-    "revision": "6066c07b58d4ef28b673",
-    "url": "/fetchnews/static/js/main.6066c07b.chunk.js"
+    "revision": "d48e417f89f211dbd31d",
+    "url": "/fetchnews/static/js/main.d48e417f.chunk.js"
   },
   {
     "revision": "adc4ac4345362923f5cc",
     "url": "/fetchnews/static/js/1.adc4ac43.chunk.js"
   },
   {
-    "revision": "6066c07b58d4ef28b673",
-    "url": "/fetchnews/static/css/main.2c8302af.chunk.css"
+    "revision": "d48e417f89f211dbd31d",
+    "url": "/fetchnews/static/css/main.d600677c.chunk.css"
   },
   {
-    "revision": "7506937d65f0a504bf442a175b162cb4",
+    "revision": "eaa224359e609244f87e9052201f7081",
     "url": "/fetchnews/index.html"
   }
 ];
