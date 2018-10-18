38783941a761fac4648ebf58e7c5df949b7f8e99
myoui <phoenixxturtle@gmail.com>
2018-10-18 13:20:34 -0700

    mobile layout, ui adjusts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom'
-import {  } from 'react-spring';
 import { Row, Column } from 'simple-flexbox';
+import { isMobile } from 'is-mobile';
+
 import { NavBar } from "./sidebar";
 import { News } from "./content";
 
@@ -9,21 +10,32 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      <Row>
-        <Column>
-          <NavBar/>
-        </Column>
-        <Column>
-          <Switch>
-            <Route exact path="/" render={() => <Redirect to="/usnews"/>}/>
-            <Route path="/:source" component={News} />
-          </Switch>
-        </Column>
-      </Row>
-      </div>
-    );
+    if (isMobile.isMobile()) {
+      return (
+        <div className="App">
+            <NavBar/>
+            <Switch>
+              <Route exact path="/" render={() => <Redirect to="/usnews"/>}/>
+              <Route path="/:source" component={News} />
+            </Switch>
+        </div>)
+    } else {
+      return (
+        <div className="App">
+        <Row>
+          <Column>
+            <NavBar/>
+          </Column>
+          <Column>
+            <Switch>
+              <Route exact path="/" render={() => <Redirect to="/usnews"/>}/>
+              <Route path="/:source" component={News} />
+            </Switch>
+          </Column>
+        </Row>
+        </div>
+      );
+    }
   }
 }
 
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { isMobile } from 'is-mobile';
-
+import { Row, Column } from 'simple-flexbox'
 class NewsLink extends Component {
     render() {
         return(
@@ -25,16 +25,43 @@ export class NavBar extends Component {
     }
 
     render() {
-        return (
-            <div className='navBar'>
-                <NewsLink link="usnews" name="Top US News"/>
-                <NewsLink link="cnn" name="CNN"/>
-                <NewsLink link="bbc" name="BBC"/>
-                <NewsLink link="washpost" name="Washington Post"/>
-                <NewsLink link="espn" name="ESPN"/>
-                <NewsLink link="engadget" name="Engadget"/>
-            </div>
-        )
+        if (!this.state.mobile) {
+            return (
+                <div className='navBar'>
+                    <NewsLink link="usnews" name="Top US News"/>
+                    <NewsLink link="cnn" name="CNN"/>
+                    <NewsLink link="bbc" name="BBC"/>
+                    <NewsLink link="washpost" name="Washington Post"/>
+                    <NewsLink link="espn" name="ESPN"/>
+                    <NewsLink link="engadget" name="Engadget"/>
+                </div>
+            )
+        } else {
+            return (
+                <div className='navBarMob'>
+                <Row>
+                    <Column>
+                        <NewsLink link="usnews" name="Top US News"/>
+                    </Column>
+                    <Column>
+                        <NewsLink link="cnn" name="CNN"/>
+                    </Column>
+                    <Column>
+                        <NewsLink link="bbc" name="BBC"/>
+                    </Column>
+                    <Column>
+                        <NewsLink link="washpost" name="Washington Post"/>
+                    </Column>
+                    <Column>
+                        <NewsLink link="espn" name="ESPN"/>
+                    </Column>
+                    <Column>
+                        <NewsLink link="engadget" name="Engadget"/>
+                    </Column>
+                </Row>
+                </div>
+            )
+        }
     }
 }
 
